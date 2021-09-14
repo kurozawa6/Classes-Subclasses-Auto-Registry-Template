6c6877dcde45dcde15cc305490625cdefe16e830
kurozawa6 <75070724+kurozawa6@users.noreply.github.com>
2021-09-14 20:31:38 +0800

    removed whitespaces beside = in arguments

--- thecode.py
+++ thecode.py
@@ -2,7 +2,7 @@ class ObjectBase:
     __subclasses = {}
     label = 'Inherited Label.'
 
-    def __init__(self, cust_parent_val1 = 'Default Custom Parent Value.',):
+    def __init__(self, cust_parent_val1='Default Custom Parent Value.',):
         self.cust_parent_val1 = cust_parent_val1
 
     def __init_subclass__(cls):
@@ -19,7 +19,7 @@ class SubObject1(ObjectBase):
     _subclass_type = 'type1'
     label = 'Label For Sub Class Type1.'
 
-    def __init__(self, child_cust_value1 = 'Default Custom Value 1.', child_mutable1 = None, **kwargs):
+    def __init__(self, child_cust_value1='Default Custom Value 1.', child_mutable1=None, **kwargs):
         super().__init__(**kwargs)
         self.child_cust_value1 = child_cust_value1
         self.child_mutable1 = child_mutable1 if child_mutable1 is not None else []
@@ -28,7 +28,7 @@ class SubObject1(ObjectBase):
 class SubObject2(ObjectBase):
     _subclass_type = 'type2'
 
-    def __init__(self, child_cust_value2 = 'default', child_mutable2 = None, **kwargs):
+    def __init__(self, child_cust_value2='default', child_mutable2=None, **kwargs):
         super().__init__(**kwargs)
         self.child_cust_value2 = child_cust_value2
         self.child_mutable2 = child_mutable2 if child_mutable2 is not None else {}
@@ -37,12 +37,12 @@ class SubObject2(ObjectBase):
 # Sample Output
 
 print('Object A:')
-print(A := ObjectBase.create('type1', cust_parent_val1 = 'Modified Parent Value1.'))
+print(A := ObjectBase.create('type1', cust_parent_val1='Modified Parent Value1.'))
 print(A.label)
 print(A.cust_parent_val1)
 print(A.child_mutable1)
 print(A.child_cust_value1 + '\n')
-print(B := ObjectBase.create('type2', child_cust_value2 = 'Modified Value 2.'))
+print(B := ObjectBase.create('type2', child_cust_value2='Modified Value 2.'))
 print('Object B:')
 print(B.label)
 print(B.cust_parent_val1)
